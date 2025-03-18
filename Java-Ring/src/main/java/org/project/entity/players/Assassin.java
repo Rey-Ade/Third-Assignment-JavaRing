5375f5b709d7896e27801daf9809ceb4f42321fe
Reyhane Adelinia <r.adelinia027@gmail.com>
2025-03-18 16:41:42 +0330

    completed Assassin character

--- Java-Ring/src/main/java/org/project/entity/players/Assassin.java
+++ Java-Ring/src/main/java/org/project/entity/players/Assassin.java
@@ -1,4 +1,46 @@
 package org.project.entity.players;
 
-public class Assassin {
+import org.project.entity.Entity;
+import org.project.object.weapons.Sword;
+import org.project.object.armors.LeatherArmor;
+
+public class Assassin extends Player{
+    // Can become invisible once per battle
+
+    private int counter = 0;
+    private boolean invisible = false;
+
+    public Assassin(String name, Sword sword, LeatherArmor leatherArmor) {
+        super(name, 100, 50, sword, leatherArmor);
+    }
+
+    @Override
+    public void attack(Entity target) {
+        if (!invisible) {
+            target.takeDamage(weapon.getDamage());
+        }
+        else if (invisible && counter < 3){
+            target.takeDamage(weapon.getDamage() + 20);
+            counter++;
+        }
+        else {
+            resetInvisible();
+        }
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        if (!invisible) {
+            super.takeDamage(damage);
+        }
+    }
+
+    public void becomeInvisible() {
+        invisible = true;
+    }
+
+    public void resetInvisible() {
+        invisible = false;
+        counter = 0;
+    }
 }
--- Java-Ring/src/main/java/org/project/object/armors/LeatherArmor.java
+++ Java-Ring/src/main/java/org/project/object/armors/LeatherArmor.java
@@ -1,4 +1,11 @@
 package org.project.object.armors;
 
-public class LeatherArmor {
+public class LeatherArmor extends Armor{
+
+    // private int manaCost = 50;
+
+    public LeatherArmor() {
+        super(40, 60);
+    }
+
 }
